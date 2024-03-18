b65b38a408565b270ba45650300c0d4cddfa98fd
mawojcik <wojcik.maciej.k@gmail.com>
2024-03-18 12:04:24 +0100

    implemented function to check if word is a palindrome, began structure for validating buffer and counting words, changed adress to localhost because of problems with connection. Set the buffer to max value udp can take

--- C/lab_3/udp_palindome_checker_server.c
+++ C/lab_3/udp_palindome_checker_server.c
@@ -6,6 +6,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
+#include <ctype.h>
 
 int serwer;
 
@@ -16,6 +17,30 @@ void koniec(void) {
         perror("close error");
         exit(1);
     }
+}
+
+int isPalindome(char word[], int len) {
+    int leftIndex = 0;
+    int rightIndex = len;
+    while (leftIndex < rightIndex) {
+        if (word[leftIndex] != word[rightIndex]) {
+            return 0;
+        }
+        leftIndex++;
+        rightIndex--;
+    }
+    return 1;
+}
+
+int bufferValidator(char *buffer) {
+    if (isspace(buffer[0])) {
+        printf("BEGGINING WITH SPACE");
+    }
+    return 0;
+}
+int palindomesCounter(char words[], int* numberOfAllWords) {
+    char word[65507] = "";
+
 }
 
 int main(int argc, char *argv[]) {
@@ -42,7 +67,8 @@ int main(int argc, char *argv[]) {
 
     struct sockaddr_in adres = {
             .sin_family = AF_INET,
-            .sin_addr.s_addr = htonl(INADDR_ANY),
+//            .sin_addr.s_addr = htonl(INADDR_ANY),
+            .sin_addr.s_addr = inet_addr("127.0.0.1"),
             .sin_port = htons(atoi(argv[1]))
     };
 
@@ -52,7 +78,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    char buffer[1024];
+    char buffer[65507] = "";
     char message[] = "Hello, world\n";
     struct sockaddr_in klient;
 
@@ -61,7 +87,22 @@ int main(int argc, char *argv[]) {
         unsigned int len = sizeof(klient);
 
         // Odebranie wiadomości z gniazdka
-        int odczyt = recvfrom(serwer, (char *)buffer, 1024, 0, (struct sockaddr *)&klient, &len);
+        int odczyt = recvfrom(serwer, (char *)buffer, 65507, 0, (struct sockaddr *)&klient, &len);
+
+//        printf("%s", buffer);
+        int numberOfAllWords = 0;
+        int numberOfPalindomes = palindomesCounter(buffer, &numberOfAllWords);
+
+        printf("%d", numberOfPalindomes);
+
+        bufferValidator(buffer);
+        printf("%s", buffer);
+
+
+
+
+
+
 
         // Sprawdzenie błędów funkcji recvfrom
         if (odczyt == -1) {
@@ -79,3 +120,8 @@ int main(int argc, char *argv[]) {
         }
     }
 }
+//int main(int argc, char *argv[]) {
+//    char input[30];
+//    scanf("%s", input);
+//    printf("%d", isPalindome(input, 3));
+//}
