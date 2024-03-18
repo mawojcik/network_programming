727a340e10d5fc8081ca219cf22c8bec42ca656c
mawojcik <wojcik.maciej.k@gmail.com>
2024-03-18 15:43:49 +0100

    ready and working server: simplified checking if word is palindrome, implemented counting words and palindromes

--- C/lab_3/udp_palindome_checker_server.c
+++ C/lab_3/udp_palindome_checker_server.c
@@ -10,18 +10,20 @@
 
 int serwer;
 
-// Funkcja własnej obsługi sygnału
 void koniec(void) {
-    // Zamknięcie połączenia
     if (close(serwer) == -1) {
         perror("close error");
         exit(1);
     }
 }
 
-int isPalindome(char word[], int len) {
+int isPalindrome(char word[]) {
     int leftIndex = 0;
-    int rightIndex = len;
+    int rightIndex = strlen(word) - 1;
+    if (rightIndex == 0) {
+        return 1;
+    }
+
     while (leftIndex < rightIndex) {
         if (word[leftIndex] != word[rightIndex]) {
             return 0;
@@ -32,34 +34,40 @@ int isPalindome(char word[], int len) {
     return 1;
 }
 
-int bufferValidator(char *buffer) {
-    if (isspace(buffer[0])) {
-        printf("BEGGINING WITH SPACE");
+int countWords(char *buffer, int *numberOfPalindomes, int *numberOfAllWords) {
+    if (buffer[0] == ' ') {
+        return -1;
     }
-    return 0;
-}
-int palindomesCounter(char words[], int* numberOfAllWords) {
-    char word[65507] = "";
+    buffer[strlen(buffer)-1] = ' ';
 
+    char *token = strtok(buffer, " ");
+
+    while (token != NULL) {
+        (*numberOfAllWords)++;
+
+        if (isPalindrome(token)) {
+            (*numberOfPalindomes)++;
+        }
+        token = strtok(NULL, " ");
+    }
+    return 1;
 }
+
 
 int main(int argc, char *argv[]) {
-    // Sprawdzenie czy podano argumenty
     if (argc != 2) {
         printf("Wrong number of arguments!");
         exit(1);
     }
-
-    // Tworzenie gniazdka
+    // Creating socket
     serwer = socket(AF_INET, SOCK_DGRAM, 0);
 
-    // Sprawdzenie czy gniazdko zostało stworzone poprawnie
+    // checking if creating failed
     if (serwer == -1) {
         perror("socket error");
         exit(1);
     }
 
-    // Rejestracja funkcji wywołanych przez exit()
     if (atexit(koniec) != 0) {
         perror("atexit error");
         exit(EXIT_FAILURE);
@@ -72,56 +80,41 @@ int main(int argc, char *argv[]) {
             .sin_port = htons(atoi(argv[1]))
     };
 
-    // Ustalenie ardesu lokalnego końca gniazdka
+    // Ustalenie adresu lokalnego końca gniazdka
     if (bind(serwer, (struct sockaddr *)&adres, sizeof(adres)) == -1) {
         perror("bind error");
         exit(1);
     }
 
-    char buffer[65507] = "";
-    char message[] = "Hello, world\n";
     struct sockaddr_in klient;
 
-    // Pętla czekająca na połączenia
     while (1) {
+        int numberOfPalindomes = 0;
+        int numberOfAllWords = 0;
+        char buffer[1024] = "";
+        char message[100] = "";
+
+
         unsigned int len = sizeof(klient);
 
-        // Odebranie wiadomości z gniazdka
-        int odczyt = recvfrom(serwer, (char *)buffer, 65507, 0, (struct sockaddr *)&klient, &len);
+        int receive_status = recvfrom(serwer, (char *)buffer, 1024, 0, (struct sockaddr *)&klient, &len);
 
-//        printf("%s", buffer);
-        int numberOfAllWords = 0;
-        int numberOfPalindomes = palindomesCounter(buffer, &numberOfAllWords);
-
-        printf("%d", numberOfPalindomes);
-
-        bufferValidator(buffer);
-        printf("%s", buffer);
-
-
-
-
-
-
-
-        // Sprawdzenie błędów funkcji recvfrom
-        if (odczyt == -1) {
+        if (receive_status == -1) {
             perror("recivefrom error");
             exit(1);
         }
 
-        // Wysłanie wiadomości do gniazdka
-        int zapis = sendto(serwer, message, strlen(message), 0, (struct sockaddr *)&klient, len);
+        if(countWords(buffer, &numberOfPalindomes, &numberOfAllWords) == -1) {
+            sprintf(message, "%s", "ERROR\n");
+        } else {
+            sprintf(message, "%d / %d\n", numberOfPalindomes, numberOfAllWords);
+        }
 
-        // Sprawdzenie błędów funkcji sendto
-        if (zapis != strlen(message)) {
+        int send_status = sendto(serwer, message, strlen(message), 0, (struct sockaddr *)&klient, len);
+        
+        if (send_status != strlen(message)) {
             perror("sendto error");
             exit(1);
         }
     }
 }
-//int main(int argc, char *argv[]) {
-//    char input[30];
-//    scanf("%s", input);
-//    printf("%d", isPalindome(input, 3));
-//}
