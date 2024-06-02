5ebb4f416e09f9d193612a29f4f427f9e39bc931
mawojcik <wojcik.maciej.k@gmail.com>
2024-06-02 23:45:17 +0200

    first part done: supporting searching with name and/or surname

--- Python/rest_webapp/rest_webapp.py
+++ Python/rest_webapp/rest_webapp.py
@@ -10,10 +10,12 @@ Uwaga: kod dydaktyczny bez pełnej obsługi błędów i sytuacji nadzwyczajnych.
 Aplikacja nie potrafi sama stworzyć swojej bazy danych, trzeba to zrobić
 przed jej uruchomieniem. Skrypt rest_webapp.sh pokazuje jak.
 '''
+from urllib.parse import parse_qs
 
 plik_bazy = './osoby.sqlite'
 
 import re, sqlite3
+
 
 class OsobyApp:
     def __init__(self, environment, start_response):
@@ -25,7 +27,7 @@ i inicjuje pola na odpowiedź.
         self.env = environment
         self.start_response = start_response
         self.status = '200 OK'
-        self.headers = [ ('Content-Type', 'text/html; charset=UTF-8') ]
+        self.headers = [('Content-Type', 'text/html; charset=UTF-8')]
         self.content = b''
 
     def __iter__(self):
@@ -40,11 +42,11 @@ ciąg bajtów do odesłania klientowi HTTP.
             s = 'SQLite error: ' + str(e)
             self.failure('500 Internal Server Error', s)
         n = len(self.content)
-        self.headers.append( ('Content-Length', str(n)) )
+        self.headers.append(('Content-Length', str(n)))
         self.start_response(self.status, self.headers)
         yield self.content
 
-    def failure(self, status, detail = None):
+    def failure(self, status, detail=None):
         '''
 Metoda wstawiająca do pól obiektu status błędu oraz dokument HTML
 z komunikatem o jego wystąpieniu.
@@ -67,12 +69,16 @@ jego zlecenie. Jest to tzw. routing zapytania.
 W niniejszej aplikacji routing jest realizowany częściowo w tej metodzie,
 a częściowo w metodach handle_table() i handle_item().
 '''
-        if self.env['PATH_INFO'] == '/osoby':
+        path_info = self.env['PATH_INFO']
+        if path_info == '/osoby':
             self.handle_table()
             return
         m = re.search('^/osoby/(?P<id>[0-9]+)$', self.env['PATH_INFO'])
         if m is not None:
             self.handle_item(m.group('id'))
+            return
+        if path_info == '/osoby/search':
+            self.handle_search()
             return
         self.failure('404 Not Found')
 
@@ -119,6 +125,42 @@ Można go pobrać, zmodyfikować, albo usunąć.
         else:
             self.failure('501 Not Implemented')
 
+    def handle_search(self):
+        query_params = parse_qs(self.env['QUERY_STRING'])
+        imie = query_params.get('imie', [None])[0]
+        nazwisko = query_params.get('nazwisko', [None])[0]
+
+        if self.env['REQUEST_METHOD'] == 'GET':
+            if imie or nazwisko:
+                colnames, rows = self.sql_select(None, imie, nazwisko)
+                self.send_rows(colnames, rows)
+            else:
+                self.failure('400 Bad Request', 'Missing search parameters: imie or nazwisko')
+        elif self.env['REQUEST_METHOD'] == 'PUT':
+            colnames, vals = self.read_tsv()
+            q = 'UPDATE osoby SET '
+            q += ', '.join([c + ' = ?' for c in colnames])
+            if imie is not None and nazwisko is not None:
+                q += ' WHERE imie = ' + str(imie) + ' AND nazwisko = ' + str(nazwisko)
+            elif imie is not None:
+                q += ' WHERE imie = ' + str(imie)
+            elif nazwisko is not None:
+                q += ' WHERE nazwisko = ' + str(nazwisko)
+            self.sql_modify(q, vals)
+            colnames, rows = self.sql_select(None, imie, nazwisko)
+            self.send_rows(colnames, rows)
+        elif self.env['REQUEST_METHOD'] == 'DELETE':
+            q = ""
+            if imie is not None and nazwisko is not None:
+                q = f"DELETE FROM osoby WHERE imie = '{imie}' AND nazwisko = '{nazwisko}'"
+            elif imie is not None:
+                q = f"DELETE FROM osoby WHERE imie = '{imie}'"
+            elif nazwisko is not None:
+                q = f"DELETE FROM osoby WHERE nazwisko = '{nazwisko}'"
+            self.sql_modify(q)
+        else:
+            self.failure('501 Not Implemented')
+
     def read_tsv(self):
         f = self.env['wsgi.input']
         n = int(self.env['CONTENT_LENGTH'])
@@ -133,37 +175,46 @@ Można go pobrać, zmodyfikować, albo usunąć.
         for row in rows:
             s += '\t'.join([str(val) for val in row]) + '\n'
         self.content = s.encode('UTF-8')
-        self.headers = [ ('Content-Type',
-                'text/tab-separated-values; charset=UTF-8') ]
+        self.headers = [('Content-Type',
+                         'text/tab-separated-values; charset=UTF-8')]
 
-    def sql_select(self, id = None):
+    def sql_select(self, id=None, imie=None, nazwisko=None):
         conn = sqlite3.connect(plik_bazy)
         crsr = conn.cursor()
         query = 'SELECT * FROM osoby'
         if id is not None:
-            query += ' WHERE id = ' + str(id)
+            query += f" WHERE id = '{id}'"
+        elif imie is not None and nazwisko is not None:
+            query += f" WHERE imie = '{imie}' AND nazwisko = '{nazwisko}'"
+        elif imie is not None and nazwisko is None:
+            query += f" WHERE imie = '{imie}'"
+        elif imie is None and nazwisko is not None:
+            query += f" WHERE nazwisko = '{nazwisko}'"
+        print(query)
         crsr.execute(query)
-        colnames = [ d[0] for d in crsr.description ]
+        colnames = [d[0] for d in crsr.description]
         rows = crsr.fetchall()
         crsr.close()
         conn.close()
         return colnames, rows
 
-    def sql_modify(self, query, params = None):
+    def sql_modify(self, query, params=None):
         conn = sqlite3.connect(plik_bazy)
         crsr = conn.cursor()
         if params is None:
             crsr.execute(query)
         else:
             crsr.execute(query, params)
-        rowid = crsr.lastrowid   # id wiersza wstawionego przez INSERT
+        rowid = crsr.lastrowid  # id wiersza wstawionego przez INSERT
         crsr.close()
         conn.commit()
         conn.close()
         return rowid
 
+
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
+
     port = 8000
     httpd = make_server('', port, OsobyApp)
     print('Listening on port %i, press ^C to stop.' % port)
